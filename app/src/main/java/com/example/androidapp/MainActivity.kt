987ee94607b6848adeadf6f415e3a3eb7713907e
Elysia <elysiahankey@gmail.com>
2025-04-04 21:06:09 +0100

    Added app icon

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -11,14 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidapp.ui.theme.AndroidAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidAppTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AndroidAppTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
